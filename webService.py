b3a69654a5f3b8394dd72dc32258032c2907b6b0
dylancreaven <g00354442@gmit.ie>
2020-12-23 19:54:46 +0000

    post method done, got prediction on html page

--- webService.py
+++ webService.py
@@ -1,5 +1,9 @@
 #flask for web service methods
 import flask as fl
+from flask import request
+import tensorflow as tf
+
+
 
 #Create a new web app.
 app = fl.Flask(__name__)
@@ -9,41 +13,12 @@ app = fl.Flask(__name__)
 def home():
     return app.send_static_file('index.html')
 
-@app.route("/sendit")
-def getPower():
-    #from EmergingTechProject.ipynb
-    # Neural networks.
-    import tensorflow.keras as kr
-
-    # Numerical arrays
-    import numpy as np
-
-    # Data frames.
-    import pandas as pd
-
-    # Plotting
-    import matplotlib.pyplot as plt
-    # Plot style.
-    plt.style.use("ggplot")
-
-    # Plot size.
-    plt.rcParams['figure.figsize'] = [14, 8]
-
-    powerData = pd.read_csv("powerproduction.csv")
-
-    # Build our model.
-    model = kr.models.Sequential()
-    model.add(kr.layers.Dense(50, input_shape=(1,), activation='sigmoid', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
-    model.add(kr.layers.Dense(1, activation='linear', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
-    model.compile('adam', loss='mean_squared_error')
-
-    model.fit(powerData['speed'], powerData['power'], epochs=300, batch_size=10)
-    #user = int(input("Enter STUFF NOW PLS: "))
-    #powerData['speed'] = user
-    #powerData['power'] = model.predict(powerData['speed'])
-    #prediction = powerData['power'][0]
-    #print("Prediction: ",prediction)
-
-
+@app.route("/powerOut", methods=["POST"])
+def powerOutput():
+    windSpeed = float(request.get_json()["value"]) #gets wind speed input from index.htmlk
+    model=tf.keras.models.load_model("powerprod.h5")#gets data model from  
+    prediction = model.predict([windSpeed]) #get prediction using data model & wind speed input
+    predList = prediction.tolist() # convert prediciton to list
+    return {'prediction':predList[0]} #get first element of prediction list
 if __name__== '__main__':
-    app.run()
+    app.run(debug=True)
